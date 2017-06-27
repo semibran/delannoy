384a3a7163884f183b46b80548ff4519fecd37dc
Brandon Semilla <semibran@gmail.com>
2017-06-27 12:57:12 -0700

    compress slightly

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const choose = require('choose')
 
 module.exports = function delannoy(m, n) {
-  var r = 0
-  var l = Math.min(m, n)
-  for (var k = 0; k <= l; k++) {
+  for (var r = 0, k = 0, l = Math.min(m, n); k <= l; k++) {
     r += choose(m + n - k, m) * choose(m, k)
   }
   return r
